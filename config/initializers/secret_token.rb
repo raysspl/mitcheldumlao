2605f4e5c2a13d769eabbf6e25dd1027dd3b63fd
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-10-15 20:07:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitcheld::Application.config.secret_key_base = 'c3fd57e90c849833acc08283994272f76f619bf92afe0444aa49e5937b8c15fff4906589667a87c623ef3ada4337f7b8bbd57618f49f5b590bda589bf80d2577'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mitcheld::Application.config.session_store :cookie_store, key: '_mitcheld_session'
